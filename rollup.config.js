ec3b0edc81ba889c4ca6b631bd227b688845aa91
p-dengbin8 <1318720440@qq.com>
2025-01-08 00:11:56 +0800

    'update'

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import typescript from 'rollup-plugin-typescript2'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import pkg from './package.json'
+
+export default {
+    input: 'src/index.ts',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs'
+        },
+        {
+            file: pkg.module,
+            format: 'es'
+        },
+        {
+            file: pkg.browser,
+            format: 'umd',
+            name: 'FileUploadHelper'
+        }
+    ],
+    plugins: [
+        typescript({
+            typescript: require('typescript')
+        }),
+        nodeResolve(),
+        commonjs()
+    ]
+} 
